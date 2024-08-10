360695cd09e19c4eb51ad89a22e0af01e9728862
O O <nkwadioo@gmail.com>
2024-08-10 11:37:20 +0200

    nativewind
    - config nativewind
    - add tailwind css classes

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,14 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora Stack.screen</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora Native wind</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}> Go to Profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
