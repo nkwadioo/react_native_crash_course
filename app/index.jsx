54c4e957a428712979b2626180b6edefdec9c0e9
O O <nkwadioo@gmail.com>
2024-08-10 11:04:29 +0200

    <Stack> <Stack.Screen> - way two
    - navigating to profile page

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Aora</Text>
+      <Text>Aora Stack.screen</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}> Go to Profile</Link>
     </View>
   );
 }
