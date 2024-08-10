ac1207490fe5584e3502cd9b0c4f4b547c4354e2
O O <nkwadioo@gmail.com>
2024-08-10 09:44:52 +0200

    rendering index.jsx page in layout
    - help in rendering different screen and specify the default one

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Slot } from 'expo-router'
 
 const RootLayout = () => {
-  return (
-    <View style={styles.container}>
-      <Text>RootLayout</Text>
-    </View>
-  )
+  return <Slot />
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
