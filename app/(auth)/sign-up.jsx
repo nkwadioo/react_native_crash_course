f72ff3d92fb03f74a93855f4a328e95d01f73a4f
O O <nkwadioo@gmail.com>
2024-08-17 04:26:48 +0200

    fix appwrite
    - connect to db correctly using /v1
    - sign up successfully

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,6 +1,6 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { images } from '../../constants'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../../components/FormField'
@@ -12,8 +12,26 @@ const SignUp = () => {
     email: '', password: '' , username: ''
   })
 
-  const submit = () => {
-    createUser()
+  const submit = async () => {
+    if(!form.username || !form.email || !form.password) {
+      return Alert.alert('Error', 'Please fill in all the fields')
+    }
+
+    setIsSubmitting(true)
+
+    try {
+      const results = await createUser(form.email, form.password, form.username)
+
+      // set it to global state using constents
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+
+
   }
   
   const [isSubmitting, setIsSubmitting] = useState(false)
