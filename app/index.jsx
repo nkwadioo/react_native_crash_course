4d80cb3eaaba09ea35df486b017e8a4407037246
O O <nkwadioo@gmail.com>
2024-08-17 09:36:43 +0200

    set up GlobalContext
    - getCurrentUser in the provider
    - provide part of the state
    - wrap the app/index with the GlobalContext
    - allow auto sign-in

--- app/index.jsx
+++ app/index.jsx
@@ -1,50 +1,57 @@
 import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import {  Image, ScrollView, Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
+import { useGlobalContext } from '../context/GlobalProvider';
 
 
 export default function App() {
-  return (
-    <SafeAreaView className="bg-primary h-full">
-        <ScrollView contentContainerStyle={{ height: '100%' }}>
-            <View className="w-full justify-center align-center min-h-[95vh] px-4">
-                <Image 
-                    source={ images.logo }
-                    className="h-[60px]"
-                    resizeMode="contain"
-                />
 
-                <Image
-                    source={ images.cards }
-                    className="max-w-[380px] w-full h-[300px]"
-                    resizeMode="contain"
-                />
+   const  {isLoading, isLoggedIn } = useGlobalContext();
 
-                <View className="relative mt-5" >
-                    <Text className="text-3xl text-white font-bold text-center">
-                        Discover Endless Possibilities with 
-                        <Text className="text-secondary-200 relative inline-block"> Aora
-                        </Text>
-                    </Text>
-                    <Image source={ images.path } className="w-[90px] h-[15px] absolute -bottom-2 right-4" />
-                </View>
+   if(!isLoading && isLoggedIn) return <Redirect href="/home" />
 
-                <Text className="font-pregular text-xs text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limited expoloration with Aora.</Text>
 
-                <View className="justify-center items-center">
-                    <CustomButton 
-                        title="Continue with Email"
-                        handlePress={() => router.push('/sign-in')}
-                        containerStyles="w-full mt-7"
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className="w-full justify-center align-center min-h-[95vh] px-4">
+                    <Image
+                        source={images.logo}
+                        className="h-[60px]"
+                        resizeMode="contain"
                     />
+
+                    <Image
+                        source={images.cards}
+                        className="max-w-[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
+
+                    <View className="relative mt-5" >
+                        <Text className="text-3xl text-white font-bold text-center">
+                            Discover Endless Possibilities with
+                            <Text className="text-secondary-200 relative inline-block"> Aora
+                            </Text>
+                        </Text>
+                        <Image source={images.path} className="w-[90px] h-[15px] absolute -bottom-2 right-4" />
+                    </View>
+
+                    <Text className="font-pregular text-xs text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limited expoloration with Aora.</Text>
+
+                    <View className="justify-center items-center">
+                        <CustomButton
+                            title="Continue with Email"
+                            handlePress={() => router.push('/sign-in')}
+                            containerStyles="w-full mt-7"
+                        />
+                    </View>
                 </View>
-            </View>
-        </ScrollView>
-        <StatusBar backgroundColor='#161622' style='light'/>
-    </SafeAreaView>
-  );
+            </ScrollView>
+            <StatusBar backgroundColor='#161622' style='light' />
+        </SafeAreaView>
+    );
 }
 
---
+++ context/GlobalProvider.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { getCurrentUser } from "../lib/appwrite";
+
+const GlobalContext = createContext()
+export const useGlobalContext = () => useContext
+(GlobalContext)
+
+const GlobalProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [isLoading, setIsLoading] = useState(true) // we fist try to load the user data
+
+    useEffect(() => {
+      getCurrentUser()
+        .then( (res) => {
+            if(res){
+                setIsLoggedIn(true)
+                setUser(res)
+            }else {
+                setIsLoggedIn(false)
+                setUser(null)
+            }
+        })
+        .catch( (error) => console.log(error))
+        .finally( () => setIsLoading(false))
+    }, [])
+    
+
+    return (
+        <GlobalContext.Provider 
+            value={{
+                isLoggedIn,
+                setIsLoggedIn,
+                user,
+                setUser,
+                isLoading
+            }}
+        >
+            {children}
+        </GlobalContext.Provider >
+    )
+}
+
+export default GlobalProvider
