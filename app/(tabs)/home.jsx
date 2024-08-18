61a78902347aaa2a062c8243d9ea65648c18e86d
O O <nkwadioo@gmail.com>
2024-08-18 21:25:06 +0200

    fetching post from appwrite

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,17 +1,47 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
+import { getAllPosts } from '../../lib/appwrite'
 
 const Home = () => {
+  const [data, setData ] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+
+      try {
+        const response = await getAllPosts();
+        setData(response)
+      } catch (error) {
+        Alert.alert('Error', error.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+  
+  console.log(data)
+
+  const [refresh, setRefresh] = useState(false)
+  const onRefresh = async () => {
+    setRefresh(true)
+    // recall videos -> if any new videos append
+    setRefresh(false)
+  }
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[ ]}
+        data={[ { id: '1'}, { id: '2'}, { id: '3'}]}
         keyExtractor={(item) => item.$id}
         renderItem={( {item} ) => (
           <Text className="3xl text-white"> {item.id} </Text>
@@ -50,8 +80,8 @@ const Home = () => {
             title="No Videos Found"
             subtitle="Be the fist one to upload a video"
           />
-        )}
-
+        )} 
+        refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}
         />
       <StatusBar style='light' />
     </SafeAreaView>
